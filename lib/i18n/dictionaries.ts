e1e351e7b211bea3beaa5510e5fac6ef8802853f
Vachers <sinanbaslar@gmail.com>
2025-11-23 05:40:47 +0300

    Add dictionary files for new languages (ru, fa, az, ar) and fix TypeScript error

--- lib/i18n/dictionaries.ts
+++ lib/i18n/dictionaries.ts
@@ -1,8 +1,12 @@
 import { Locale } from "./config";
 
-const dictionaries = {
+const dictionaries: Record<Locale, () => Promise<any>> = {
   tr: () => import("./locales/tr.json").then((module) => module.default),
   en: () => import("./locales/en.json").then((module) => module.default),
+  ru: () => import("./locales/ru.json").then((module) => module.default),
+  fa: () => import("./locales/fa.json").then((module) => module.default),
+  az: () => import("./locales/az.json").then((module) => module.default),
+  ar: () => import("./locales/ar.json").then((module) => module.default),
 };
 
 export const getDictionary = async (locale: Locale) => {
